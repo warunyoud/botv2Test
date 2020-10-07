fb38b0996e5d4abcb5ef3c5de4e2daec76bdf9c9
Marco Giuliani <thebaffo@gmail.com>
2020-10-07 16:54:45 +0700

    add 'searchWorkflowTemplate' command

--- index.js
+++ index.js
@@ -57,6 +57,10 @@ const createResponse = (responseMap, text) => {
   return response;
 }
 
+/*
+ * Workflows
+ */
+
 const createWorkflowResponse = (workflows) => {
   const response = Object.assign({}, workflowTemplate);
 
@@ -66,7 +70,7 @@ const createWorkflowResponse = (workflows) => {
         action: {
             type: "workflow",
             workflowId: workflow._id,
-            label: workflow.title,
+            label: trimLabel(workflow.title),
         }
     }
   });
@@ -80,15 +84,74 @@ const createWorkflowResponse = (workflows) => {
   return [response];
 }
 
-const searchWorkflow = async (client, replyToken, userId, keyword) => {
-  const endpoint = 'api/workflow/v1/users/' + userId + '?keyword=' + keyword;
-  const response = await client.instance.get(endpoint);
-  const data = await response.data;
-  const workflows = data.workflows || [];
+const searchWorkflow = async (client, userId, keyword) => {
+  try {
+    const endpoint = 'api/workflow/v1/users/' + userId + '?keyword=' + keyword;
+    const response = await client.instance.get(endpoint);
+    const data = await response.data;
 
-  const botResponse = createWorkflowResponse(workflows)
-  client.replyV2(replyToken, botResponse);
+    return data.workflows || [];
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 }
+
+/*
+ * Workflow Templates
+ */
+
+const createWorkflowTemplateResponse = (templates) => {
+  const response = Object.assign({}, workflowTemplate);
+
+  const items = templates.map(template => {
+    return {
+        type: "action",
+        action: {
+            type: "workflowTemplate",
+            templateId: template._id,
+            label: trimLabel(template.name),
+        }
+    }
+  });
+
+  response.quickReply.items = items;
+
+  if (items.length === 0) {
+    response.text = "We couldn't find any workflow that matched the search criteria";
+  }
+
+  return [response];
+}
+
+const searchWorkflowTemplate = async (client, keyword) => {
+  try {
+    const endpoint = 'api/workflow/v1?keyword=' + keyword;
+    const response = await client.instance.get(endpoint);
+    const data = await response.data;
+    
+    return data.templates || [];
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+/*
+ * Utils
+ */
+
+const trimLabel = (label) => {
+  if (label && label.length >= 20) {
+    return label.substring(0, 17) + '...';
+  }
+
+  return label;
+}
+
+/*
+ * Middleware
+ */
 
 async function middleware (params, path) {
   try {
@@ -120,7 +183,21 @@ async function middleware (params, path) {
             const [_, keyword] = message.text.split(' ');
             const userId = event.source.userId;
 
-            searchWorkflow(clients[path], replyToken, userId, keyword)
+            // Retrieve workflow from API
+            const workflows = await searchWorkflow(clients[path], userId, keyword);
+
+            // Send reply
+            const response = createWorkflowResponse(workflows)
+            clients[path].replyV2(replyToken, response);
+          } else if (/\/searchWorkflowTemplate\s\w+$/.test(message.text)) {
+            const [_, keyword] = message.text.split(' ');
+
+            // Retrieve workflow from API
+            const templates = await searchWorkflowTemplate(clients[path], keyword);
+
+            // Send reply
+            const response = createWorkflowTemplateResponse(templates);
+            clients[path].replyV2(replyToken, response);
           } else {
             clients[path].replyV2(replyToken, createResponse(responseMap, message.text));
           }
